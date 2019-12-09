9a31f5dc6174cb2dcbce04eded01351c556e2f1e
tom <stavrosatic@gmail.com>
2019-12-09 23:43:05 +0800

    新增gd32f130支持

--- firmware_bootloader/make.bat
+++ firmware_bootloader/make.bat
@@ -10,11 +10,13 @@ echo 4. STM32F103RE
 echo 5. STM32F303CC
 echo 6. STM32F401RC
 echo 7. GD32F350CB 
+echo 8. GD32F130F8
 echo Q. Exit
 echo Please enter:
-choice /c:1234567Q
+choice /c:12345678Q
 
-if errorlevel 8 goto :end
+if errorlevel 9 goto :end
+if errorlevel 8 make.exe -Bsj -f "_makefiles\Makefile.gd32f130_150"
 if errorlevel 7 make.exe -Bsj -f "_makefiles\Makefile.gd32f350"
 if errorlevel 6 make.exe -Bsj -f "_makefiles\Makefile.stm32f401"
 if errorlevel 5 make.exe -Bsj -f "_makefiles\Makefile.stm32f303"
