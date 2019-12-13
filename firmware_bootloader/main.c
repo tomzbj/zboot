d091cc6ab04a19077399362aec59621b9649100c
tom <stavrosatic@gmail.com>
2019-12-13 22:16:38 +0800

    removed 2 warnings

--- firmware_bootloader/main.c
+++ firmware_bootloader/main.c
@@ -12,7 +12,7 @@ void SystemInit(void)
 #if defined (GD32F350) || defined (GD32F130_150)
 //    rcu_deinit();
     RCU_APB2EN = BIT(0);
-    RCU_CTL0 |= RCU_CTL0_IRC8MEN; // enable IRC8M
+    RCU_CTL0 |= RCU_CTL0_IRC8MEN;// enable IRC8M
     ( {  while(RESET == (RCU_CTL0 & RCU_CTL0_IRC8MSTB));}); // reset RCU
     RCU_CFG0 = 0x00000000;
     RCU_CFG1 = 0x00000000;
@@ -45,29 +45,30 @@ void SystemInit(void)
 }
 
 #if defined (STM32F10X_USART1_FORK)
-int GPIO_Config(){
-  RCC->APB2ENR |= RCC_APB2Periph_GPIOC;
-  GPIOC->CRL &= ~(0xf << (2*4));
-  GPIOC->CRL |= (0b0001 << (2*4)); /* output pp */
-  GPIOC->BSRR |= 2 << 16;	/* set PC2 low to mute beep on board*/
+int GPIO_Config() {
+    RCC->APB2ENR |= RCC_APB2Periph_GPIOC;
+    GPIOC->CRL &= ~(0xf << (2*4));
+    GPIOC->CRL |= (0b0001 << (2*4)); /* output pp */
+    GPIOC->BSRR |= 2 << 16; /* set PC2 low to mute beep on board*/
 
-  RCC->APB2ENR |= RCC_APB2Periph_GPIOB;
-  GPIOB->CRH &= ~(0xf << (15-8)*4);
-  GPIOB->CRH |= (GPIO_Mode_IPU&0xf) << ((15-8)*4); /* PB15: input pull-up-down */
-  GPIOB->BSRR |= 1 << 15;			   /* set PB15 to pull up */
+    RCC->APB2ENR |= RCC_APB2Periph_GPIOB;
+    GPIOB->CRH &= ~(0xf << (15-8)*4);
+    GPIOB->CRH |= (GPIO_Mode_IPU&0xf) << ((15-8)*4); /* PB15: input pull-up-down */
+    GPIOB->BSRR |= 1 << 15; /* set PB15 to pull up */
 }
 
-int Button_Pushed(){
-  return (GPIOB->IDR & (1<< 15)) ? 0: 1;
+int Button_Pushed(void) {
+    return (GPIOB->IDR & (1<< 15)) ? 0: 1;
 }
 #else
-int Button_Pushed(){ return 0;}	/* always False by default */
+int Button_Pushed(void)
+{
+    return 0;
+} /* always False by default */
 #endif
-
 
 int main(void)
 {
-    volatile int i=0;
     SystemInit();
     SysTick_Config(8000000UL); // delay 1s and then jump to app
 
@@ -75,7 +76,7 @@ int main(void)
     /* add by Lv: init gpio for better jump control  */
     GPIO_Config();
 #endif
-    
+
     IAP_Config();
     USART_Config();
     IAP_Sysinfo_t* inf = IAP_GetInfo();
@@ -85,10 +86,10 @@ int main(void)
 #endif
     while(1) {
         USART_Poll();
-        if(flag_jump){
-	    // xprintf("jump\n");
-	    IAP_JumpToApp();
-	}
+        if(flag_jump) {
+            // xprintf("jump\n");
+            IAP_JumpToApp();
+        }
     }
 }
 
